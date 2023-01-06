d2162253de5cd5fd130100aa4c120a5c348c3d2e
Brayton-Lordianto <braylord30@gmail.com>
2023-01-05 17:39:08 -0800

    Initial Commit

---
+++ Stacker1/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Stacker1
+//
+//  Created by Brayton Lordianto on 1/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stacker1/Stacker1App.swift
@@ -0,0 +1,17 @@
+//
+//  Stacker1App.swift
+//  Stacker1
+//
+//  Created by Brayton Lordianto on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Stacker1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
